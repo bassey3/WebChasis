2f23b8d6ed137b0382a575244e345802af0f2533
Lawrence Bassey-Oden <bassey3@gmail.com>
2018-11-16 11:24:38 +0100

    Initial commit

---
+++ src/main/java/com/exgnt/web/Dashboard.java
@@ -0,0 +1,17 @@
+package com.exgnt.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Dashboard {
+
+    @GetMapping("/dashboard")
+    public String dashboard(@RequestParam(name="name", required=false, defaultValue="Fred") String name, Model model) {
+        model.addAttribute("name", name);
+        System.out.println("name");
+        return "index5";
+    }
+}
